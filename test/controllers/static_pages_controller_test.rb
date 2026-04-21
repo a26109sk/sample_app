b30a1c48c26bd8eb7ca10192487aff3a8d0a3f57
a26109sk <a26109sk@aiit.ac.jp>
2026-04-21 09:19:39 +0000

    3.4.4

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -9,7 +9,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-     assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
@@ -22,5 +22,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_about_url
     assert_response :success
     assert_select "title", "About | #{@base_title}"
+  end
+
+    test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 end
